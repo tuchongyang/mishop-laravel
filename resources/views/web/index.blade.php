8ddc54f5e1b690b3fd1639a68de551a05228ab4d
tuchongyang <779311998@qq.com>
2021-01-28 07:09:47 +0800

    update index

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -8,7 +8,40 @@
     <div class="container">
         <div class="category-item">
             <div class="title">手机</div>
-            <div class="product-list" style="height:460px;background:#eee;"></div>
+            <div class="product-list" style="height:460px;background:#eee;">
+                <div class="item">
+                    <a href="javascript:;">
+                    <div class="figure-img"><img src="//cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg?thumb=1&w=200&h=200&f=webp&q=90"></div>
+                    <h3 class="title">小米手机11</h3>
+                    <p class="desc">骁龙888 | 2K四曲面屏</p>
+                    <p class="price">3999元起</p>
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="javascript:;">
+                    <div class="figure-img"><img src="//cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg?thumb=1&w=200&h=200&f=webp&q=90"></div>
+                    <h3 class="title">小米手机11</h3>
+                    <p class="desc">骁龙888 | 2K四曲面屏</p>
+                    <p class="price">3999元起</p>
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="javascript:;">
+                    <div class="figure-img"><img src="//cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg?thumb=1&w=200&h=200&f=webp&q=90"></div>
+                    <h3 class="title">小米手机11</h3>
+                    <p class="desc">骁龙888 | 2K四曲面屏</p>
+                    <p class="price">3999元起</p>
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="javascript:;">
+                    <div class="figure-img"><img src="//cdn.cnbj1.fds.api.mi-img.com/mi-mall/5a260090e0e08770b0bd865845a4b4ab.jpg?thumb=1&w=200&h=200&f=webp&q=90"></div>
+                    <h3 class="title">小米手机11</h3>
+                    <p class="desc">骁龙888 | 2K四曲面屏</p>
+                    <p class="price">3999元起</p>
+                    </a>
+                </div>
+            </div>
         </div>
         <div class="category-item">
             <div class="title">手机</div>
